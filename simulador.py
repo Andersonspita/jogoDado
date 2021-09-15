cdf28b9e81e777f6a332268dc9c78902f9623596
Andersonspita <andersonspita@hotmail.com.br>
2021-09-15 10:53:36 -0300

    Add files via upload
    Atualização do simulador de um dado

--- simulador.py
+++ simulador.py
@@ -3,16 +3,19 @@ from random import randint
 print("{:=^40}" .format(" JOGO DE DADOS "))
 
 dado = randint(1, 7)
+opcao = 1
 
 print('''VOCÊ GOSTARIA DE JOGAR O DADO?
 [ 1 ] SIM
 [ 2 ] NÃO''')
 opcao = int(input("Escolha sua opção: "))
 
+while opcao != 1 and opcao != 2:
+    print("OPÇÃO INVÁLIDA! Escolha uma nova opção!")
+    opcao = int(input("Escolha sua opção: "))
 if opcao == 1:
-    print("Você jogou o dado e seu número é {}" .format(dado))
+    while opcao == 1:
+        print("Você jogou o dado e seu número é {}" .format(dado))
+        opcao = int(input("Escolha uma nova opção: "))
 elif opcao == 2:
     print("Você escolheu sair, obrigado e volte sempre!")
-else:
-    print("OPÇÃO INVÁLIDA! Escolha uma nova opção!")
-
